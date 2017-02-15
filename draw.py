efe6f0094f76035bdf710121f87cacb1806c29d3
Kevin Zhang <kevinzhang3702@gmail.com>
2017-02-14 22:28:55 -0500

    finished work

--- draw.py
+++ draw.py
@@ -1,93 +1,24 @@
 from display import *
 
-'''
-##Octant 1 
-def draw_line( screen, x0, y0, x1, y1, color ):
-
-    A = y1 - y0
-    B = x0 - x1
-
-    x = x0
-    y = y0
-
-    d = 2*A + B
-    A = 2*A
-    B = 2*B
-
-    while x < x1:
-        #print( "plotting at (%d, %d)"%(x,y))
-        plot( screen, color, x, y)
-        if ( d > 0 ):
-            y+=1
-            d+=B
-        x+=1
-        d+=A
-'''
-
-'''
-##Octant 2
-def draw_line( screen, x0, y0, x1, y1, color ):
-
-    A = y1 - y0
-    B = x0 - x1
-
-    x = x0
-    y = y0
-
-    d = 2*A + B
-    A = 2*A
-    B = 2*B
-
-    while y < y1:
-        #print( "plotting at (%d, %d)"%(x,y))
-        plot( screen, color, x, y)
-        if ( d < 0 ):
-            x+=1
-            d+=A
-        y+=1
-        d+=B 
-
-'''
-
-'''
-##Octant 8
-def draw_line( screen, x0, y0, x1, y1, color ):
-
-    A = y1 - y0
-    B = x0 - x1
-
-    x = x0
-    y = y0
-
-    d = 2*A + B
-    A = 2*A
-    B = 2*B
-
-    while x < x1:
-        #print( "plotting at (%d, %d)"%(x,y))
-        plot( screen, color, x, y)
-        if ( d > 0 ):
-            y-=1
-            d+=B
-        x+=1
-        d-=A 
-'''
-
 def draw_line( screen, x0, y0, x1, y1, color):
     
     A = y1 - y0
     B = x0 - x1
 
+    if( x0 > x1 ):
+        draw_line( screen, x1, y1, x0, y0, color)
+        return 0
+    
     x = x0
     y = y0
 
-    d = 2*A + B
-    A = 2*A
-    B = 2*B
-
-    m = ( A / -B)
-    if ( m >= 0 ):
-        if ( m <= 1):
+    if ( A >= 0 ):
+        if ( -B >= A):
+            #Octant 1
+            d = 2*A + B
+            A = 2*A
+            B = 2*B
+            print("Octant 1")
             while x < x1:
                 #print( "plotting at (%d, %d)"%(x,y))
                 plot( screen, color, x, y)
@@ -97,29 +28,44 @@ def draw_line( screen, x0, y0, x1, y1, color):
                 x += 1
                 d += A
         else:
+            #Octant 2
+            d = A + 2*B
+            A = 2*A
+            B = 2*B
+            print("Octant 2")
             while y < y1:
                 #print( "plotting at (%d, %d)"%(x,y))
                 plot( screen, color, x, y)
                 if ( d < 0 ):
-                    x+=1
-                    d+=A
-                y+=1
-                d+=B
+                    x += 1
+                    d += A
+                y += 1
+                d += B
     else:
-        if( m >= -1):
+        if( B <= A):
+            #Octant 8
+            d = 2*A - B
+            A = 2*A
+            B = 2*B
+            print("Octant 8")
             while x < x1:
                 #print( "plotting at (%d, %d)"%(x,y))
                 plot( screen, color, x, y)
-                if ( d > 0 ):
-                    y-=1
-                    d+=B
-                x+=1
-                d-=A
+                if ( d < 0 ):
+                    y -= 1
+                    d -= B
+                x += 1
+                d += A
         else:
-            while y < y1:
+            #Octant 7
+            d = A - 2*B
+            A = 2*A
+            B = 2*B
+            print("Octant 7")
+            while y > y1:
                 plot( screen, color, x, y)
-                if( d < 0):
+                if( d > 0):
                     x += 1
-                    d -= A
+                    d += A
                 y -= 1
-                d += B
+                d -= B
--- main.py
+++ main.py
@@ -4,23 +4,15 @@ from draw import *
 screen = new_screen()
 color = [ 255, 255, 255 ]
 
-colorRange = [ 10, 70, 150, 230 ]
+draw_line(screen, 0, 0, 250, 500, color )
+draw_line(screen, 250, 500, 500, 0, color)
+draw_line(screen, 500, 0, 0, 250, color)
+draw_line(screen, 0, 250, 500, 500, color)
+draw_line(screen, 500, 500, 250, 0, color)
+draw_line(screen, 250, 0, 0, 500, color)
+draw_line(screen, 0, 500, 500, 250, color)
+draw_line(screen, 500, 250, 0, 0, color)
 
-x = 500
-y = 0
-
-for color1 in colorRange:
-    for color2 in colorRange:
-        for color3 in colorRange:
-            color[RED] = color1
-            color[BLUE] = color2
-            color[GREEN] = color3
-            
-            draw_line(screen, 0, 0, x, y, color )
-            y += 30
-
-
-#draw_line(screen, 0, 500, 500, 250, color )
 
 #draw_line(screen, 0, 250, 500, 250, color)
 #draw_line(screen, 120, 250, 140, 170, color)
